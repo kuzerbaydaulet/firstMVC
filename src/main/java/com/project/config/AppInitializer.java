5d57442b67404a589ac7d516cfb233760207c863
kuzerbaydaulet <kuzerbaidaulet@gmail.com>
2020-02-11 16:01:29 +0600

    Commit 2

---
+++ src/main/java/com/project/config/AppInitializer.java
@@ -0,0 +1,17 @@
+package com.project.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebMVCConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
